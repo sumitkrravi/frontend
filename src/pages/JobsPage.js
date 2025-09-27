a8d3a65fbb0b70e0ea2d2627e923329d4a4024b8
sumitkrravi <ravisumit1425@gmail.com>
2025-09-27 16:22:09 +0530

    refactor JobsPage layout to include Latest Jobs, Admit Card, Result, Scholarships, Important Links, and Admission sections

--- src/pages/JobsPage.js
+++ src/pages/JobsPage.js
@@ -21,29 +21,132 @@ export default function JobsPage() {
         ))}
       </div>
 
-      {/* Sections */}
-      {jobsData.sections.map((section, idx) => (
-        <div key={idx} className="mb-5">
-          <h3 className="mb-3">{section.category}</h3>
-
-          <ul className="list-group">
-            {section.posts.map((post) => (
-              <li key={post.id} className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                <div>
-                  <h6 className="mb-1">{post.title}</h6>
-                  <p className="mb-1 text-muted">{post.desc}</p>
-                  <small>Date: {post.date}</small>
-                </div>
-                <div className="mt-2 mt-md-0">
-                  <Link to={`/jobs/${post.id}`} className="btn btn-primary">
-                    View Details
-                  </Link>
-                </div>
+      {/* first Sections */}
+      <div className="row">
+        {/* Latest Jobs */}
+        <div className="col-md-4 mb-3">
+          <div className="card border-danger">
+            <div className="card-header bg-danger text-white text-center fw-bold">
+              Latest Jobs
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <a href="#">IBPS RRB 14th Recruitment 2025 Date Extend</a>
               </li>
-            ))}
-          </ul>
+              <li className="list-group-item">
+                <a href="#">Railway RRC ECR Patna Apprentice Form 2025</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">SSC Delhi Police Driver Online Form</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      ))}
+
+        {/* Admit Card */}
+        <div className="col-md-4 mb-3">
+          <div className="card border-danger">
+            <div className="card-header bg-danger text-white text-center fw-bold">
+              Admit Card
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <a href="#">IB Security Assistant Admit Card 2025</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">LIC AAO / AE Admit Card 2025</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">RPSC Assistant Engineer Admit Card 2025</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Result */}
+        <div className="col-md-4 mb-3">
+          <div className="card border-danger">
+            <div className="card-header bg-danger text-white text-center fw-bold">
+              Result
+            </div>
+            <ul className="list-group list-group-flush">
+              <Link className="list-group-item">
+                <a href="/jobs/4">BPSSC Bihar Police Enforcement SI Pre Result 2025</a>
+              </Link>
+              <li className="list-group-item">
+                <a href="#">Bihar Police Range Officer Pre Result 2025</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">Bihar Police SI Mains Result 2025</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* second Sections */}
+      <div className="row">
+        {/*  Scholorships */}
+        <div className="col-md-4 mb-3">
+          <div className="card border-danger">
+            <div className="card-header bg-danger text-white text-center fw-bold">
+              Scholorships            
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <a href="#">Bihar Scholarship Online Form 2024</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">Jharkhand Scholarship Online Form 2024</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">UP Scholarship Online Form 2024</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Important links */}
+        <div className="col-md-4 mb-3">
+          <div className="card border-danger">
+            <div className="card-header bg-danger text-white text-center fw-bold">
+              Important Links
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <a href="#">MP Rojgar Panjiyan 2025</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">NTA UGC National Eligibility Test NET / JRF Verificantion</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">RPSC Assistant Engineer Admit Card 2025</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Admission */}
+        <div className="col-md-4 mb-3">
+          <div className="card border-danger">
+            <div className="card-header bg-danger text-white text-center fw-bold">
+              Admission
+            </div>
+            <ul className="list-group list-group-flush">
+              <Link className="list-group-item">
+                <a href="/jobs/4">NTA Common University Admission Test CUET UG 2025</a>
+              </Link>
+              <li className="list-group-item">
+                <a href="#">NAT Board NBE Graduate Pharmacy Aptitude Test GPAT 2025</a>
+              </li>
+              <li className="list-group-item">
+                <a href="#">IIT GATE Admission Online Form 2026</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
